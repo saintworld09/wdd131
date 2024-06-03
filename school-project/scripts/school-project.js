dce5496ba2b422074f839771241d939ba24a0e33
saintworld09 <ababatunde306@gnail.com>
2024-06-03 08:34:53 -0700

    week 5 project assignment

--- school-project/scripts/school-project.js
+++
@@ -1,8 +0,0 @@
-// school-project.js
-
-// JavaScript code for adding dynamic elements and interactivity
-
-// Example: Display current date in footer
-const currentDate = new Date();
-const footer = document.querySelector('footer');
-footer.innerHTML += `<p>Current Date: ${currentDate.toDateString()}</p>`;
